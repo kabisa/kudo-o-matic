85ed9d3e91f27f0d0d36f40aed04c7c8c02fe79c
Ariejan de Vroom <ariejan@ariejan.net>
2016-03-30 14:41:51 +0200

    Fix user -> transaction associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :sent_transactions, foreign_key: :from_id
-  has_many :received_transactions, foreign_key: :to_id
+  has_many :sent_transactions,      class_name: "Transaction", foreign_key: :from_id
+  has_many :received_transactions,  class_name: "Transaction", foreign_key: :to_id
 
 end
