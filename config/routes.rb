27b8216fe98106c7413cc5dc6982828cb1507e26
Ariejan de Vroom <ariejan@ariejan.net>
2016-03-30 11:58:39 +0200

    Add administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :balances
+resources :goals
+resources :transactions
+resources :activities
+resources :users
+
+    root to: "balances#index"
+  end
+
   root 'dashboard#index'
 end
