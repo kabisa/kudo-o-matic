e3812beb5ef21b93dc7a13bef0a26d5beb0744a4
Ariejan de Vroom <ariejan@ariejan.net>
2016-03-31 16:33:09 +0200

    Add transactions#new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+
+  get 'transactions/new'
+
+  # Administrate
   namespace :admin do
     resources :balances
-resources :goals
-resources :transactions
-resources :activities
-resources :users
+    resources :goals
+    resources :transactions
+    resources :activities
+    resources :users
 
     root to: "balances#index"
   end
+
+  resources :transactions,
+    only: [:new]
 
   root 'dashboard#index'
 end
