e2404d25181756de2303740b7b90ee93ba79b39b
Canolkabisa <canol.dogan@kabisa.nl>
2016-11-18 14:15:34 +0100

    use classes instead of ids

--- app/assets/javascripts/jar.js
+++ app/assets/javascripts/jar.js
@@ -1,8 +1,8 @@
 var worker = null;
 var loadedPercentage = 0;
 
-function increment(jar) {
-    $('#counter').html(jar.balanceCoins + ' Coins');
+function increment() {
+    $('#counter').html(loader.balanceCoins + ' Coins');
     $('#drink').css('top', (100-loadedPercentage*.9)+'%');
 
     if(loadedPercentage % 5 == 0){
@@ -10,9 +10,9 @@ function increment(jar) {
         $('#cubes div:nth-child(' + slice +')').fadeIn(100)
     }
 
-    if(loadedPercentage == jar.balancePercentage) {
+    if(loadedPercentage == loader.balancePercentage) {
         loadedPercentage = 0;
-        stopLoading(jar.balancePercentage);
+        stopLoading(loader.balancePercentage);
         $(".dynamic").fadeIn(1000);
         $(".loadingmessage").hide();
     }
@@ -23,31 +23,32 @@ function increment(jar) {
 
 function startLoading(bp) {
     $('#cubes div').hide();
-
-    worker = setInterval(function(){increment(bp)} , 60);
+    worker = setInterval(increment , 60);
 }
 function stopLoading(bp) {
+    console.log("stoploading")
     clearInterval(worker);
     setText(bp)
 }
 
 function setText(bp){
     if (bp >= 100){
-      $(".dynamicmessage").html("5th message ");
+        $(".dynamicmessage").html("5th message ");
     }else if (bp > 80){
-      $(".dynamicmessage").html("4th message ");
+        $(".dynamicmessage").html("4th message ");
     }else if (bp > 60){
-      $(".dynamicmessage").html("3rd message");
+        $(".dynamicmessage").html("3rd message");
     }else if (bp > 40){
-      $(".dynamicmessage").html("2nd message ");
+        $(".dynamicmessage").html("2nd message ");
     }else if (bp > 20){
-      $(".dynamicmessage").html("1st message ");
+        $(".dynamicmessage").html("1st message ");
     }
 };
 
 $(document).ready(function(){
     $(".dynamic").hide();
-    startLoading($('#loader').data())
+    window.loader = $('.loader').data()
+    startLoading();
 })
 
 
