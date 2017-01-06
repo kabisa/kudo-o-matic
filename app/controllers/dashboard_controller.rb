0a3a675d667364b057026616475eaf884727cd04
remcohh <remcohh@gmail.com>
2017-01-06 09:42:01 +0100

    Use kaminari for pagination since it was allready there

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,12 +5,8 @@ class DashboardController < ApplicationController
     @previous = Goal.previous.decorate
     @next     = Goal.next.decorate
     @goals    = Goal.all.order(:cached_votes_up => :desc)
-
     @balance  = Balance.current.decorate
-
-    @transactions = Transaction.order('created_at desc').paginate(page: params[:page], per_page: 20)
+    @transactions = Transaction.order('created_at desc').page(params[:page]).per(20)
     @transaction = Transaction.new
-
-
   end
 end
