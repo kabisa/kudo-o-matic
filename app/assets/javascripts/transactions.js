62223104c85b43fc61b61815a346e1be04a47697
Egon Meijers <egonmeijers12@gmail.com>
2017-03-21 09:55:06 +0100

    Deleted url helpers and blank line

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -4,7 +4,6 @@ jQuery(function() {
         autoFocus: true,
         maxShowItems: 5
     });
-    
 });
 
 jQuery(function() {
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  Rails.application.routes.url_helpers
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
