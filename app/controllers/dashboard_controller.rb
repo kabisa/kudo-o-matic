77ea34481c816d584bc1e65cdda54563acee612f
Egon Meijers <egonmeijers12@gmail.com>
2017-04-04 11:23:20 +0200

    Added kudo counter, minor css changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,7 @@ class DashboardController < ApplicationController
   def index
     @previous = Goal.previous.decorate
     @next     = Goal.next.decorate
+    @iterate_goals  = Goal.goals
     @goals    = Goal.all.order(:cached_votes_up => :desc)
     @balance  = Balance.current.decorate
     @transactions = Transaction.order('created_at desc').page(params[:page]).per(20)
