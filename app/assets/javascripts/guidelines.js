dd9637cb132027afbcf47f2e75f659133caa0297
Egon Meijers <egonmeijers12@gmail.com>
2017-04-18 09:38:40 +0200

    Added guideline suggestions

---
+++ app/assets/javascripts/guidelines.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.suggested-guidelines-container').hide();
+
+    $('#transaction_amount').on('keyup', function () {
+        if (this.value) {
+            $('.suggested-guidelines-container').slideDown(250);
+        }
+
+        $('.amount-of-kudos').html(this.value);
+
+        $.get("/kudo_guidelines?kudo_amount=" + this.value, function (data) {
+            $(".suggested-guidelines").html(data);
+            $('.suggested-guidelines').each(function () {
+                // debugger
+                var $this = $(this);
+                for (var i = 0; i < data.length; i++) {
+                    data[i] = wrap_activity_in_li(data[i])
+                }
+                $this.html($('<ul>').html(data));
+            });
+        })
+    });
+
+    function wrap_activity_in_li(activity){
+        return activity = '<li>' + activity[0] + '<span class="highlighted"> ' +  activity[1] + '</span>' +'</li>';
+    }
+
+    $('#transaction_amount').keyup(function(){
+        if (!this.value) {
+            $('.suggested-guidelines-container').slideUp(250);
+        }
+    });
+});
