1b97da254dcbdfa0c0dfa75b546fd8ef276abcae
Egon Meijers <egonmeijers12@gmail.com>
2017-05-29 10:47:06 +0200

    Precompile assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.config.assets.precompile += %w(*.png)
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
