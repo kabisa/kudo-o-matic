2ce377a97284faa966e13f0d42e88560aace6bea
Egon Meijers <egonmeijers12@gmail.com>
2017-06-02 13:53:20 +0200

    Filter temporary turned off

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     // Checkbox icons toggle
-    $('.filter-option').click(function () {
-        $(this).find('i').toggleClass('fa-square-o fa-check-square-o')
-    });
+    // $('.filter-option').click(function () {
+    //     $(this).find('i').toggleClass('fa-square-o fa-check-square-o')
+    // });
 
     // $('.filter-option').click(function () {
     //     if ($('.filter-option.send i').hasClass('fa-check-square-o') && $('.filter-option.received i').hasClass('fa-check-square-o')) {
@@ -32,39 +32,39 @@ $(document).ready(function() {
     //     }
     // });
 
-    $('.user-statistics-container').click(function () {
-        $('.delete-filter').addClass('button-enabled');
-    });
-
-    $('.given-transactions').click(function () {
-        $.ajax({
-            type:'GET',
-            url: "/transactions.js?filter=send", success: function (result) {
-            }
-        });
-    });
-
-    $('.received-transactions').click(function () {
-        $.ajax({
-            type:'GET',
-            url: "/transactions.js?filter=received", success: function (result) {
-            }
-        });
-    });
-
-    $('.all-transactions').click(function () {
-        $.ajax({
-            type:'GET',
-            url: "/transactions.js?filter=mine", success: function (result) {
-            }
-        });
-    });
-
-    $('.delete-filter').click(function () {
-        $.ajax({
-            type:'GET',
-            url: "/transactions.js?filter=all", success: function (result) {
-            }
-        });
-    });
+    // $('.user-statistics-container').click(function () {
+    //     $('.delete-filter').addClass('button-enabled');
+    // });
+    //
+    // $('.given-transactions').click(function () {
+    //     $.ajax({
+    //         type:'GET',
+    //         url: "/transactions.js?filter=send", success: function (result) {
+    //         }
+    //     });
+    // });
+    //
+    // $('.received-transactions').click(function () {
+    //     $.ajax({
+    //         type:'GET',
+    //         url: "/transactions.js?filter=received", success: function (result) {
+    //         }
+    //     });
+    // });
+    //
+    // $('.all-transactions').click(function () {
+    //     $.ajax({
+    //         type:'GET',
+    //         url: "/transactions.js?filter=mine", success: function (result) {
+    //         }
+    //     });
+    // });
+    //
+    // $('.delete-filter').click(function () {
+    //     $.ajax({
+    //         type:'GET',
+    //         url: "/transactions.js?filter=all", success: function (result) {
+    //         }
+    //     });
+    // });
 });
