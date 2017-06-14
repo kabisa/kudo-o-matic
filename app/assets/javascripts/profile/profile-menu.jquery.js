7e1a9f51c20519057afd569f588c323295332e70
Egon Meijers <egonmeijers12@gmail.com>
2017-06-14 16:22:19 +0200

    Fixed issue with display tooltips in IE Fixed issue #146 by changing .container-left flex property

--- app/assets/javascripts/profile/profile-menu.jquery.js
+++ app/assets/javascripts/profile/profile-menu.jquery.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
     // Dropdown profile toggle show/hide
     $('.current-user').click(function () {
         var innerWidth = $('.current-user').innerWidth();
+        var innerWidthMobile = $('.profile-picture-mobile').innerWidth();
         $('.dropdown-content').toggleClass('slide-menu');
         $('.drop-menu .menu-tooltip').toggleClass('show-tooltip');
 
         if ($(window).width() > 720) {
-            $('.dropdown-content').css({width: innerWidth});
+            $('.dropdown-content').css({width: innerWidth - 50});
         } else {
-            $('.dropdown-content').css({width: 'fit-content'});
+            $('.dropdown-content').css({width: innerWidthMobile / 2});
         }
         return false
     });
