a4eb4066fe02a83c5281381f9577fafca9d4f37c
Egon Meijers <egonmeijers12@gmail.com>
2017-06-22 09:25:25 +0200

    Create new file in FactoryGirl image to fix tests

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :transaction do
-    sender 
+    sender
     receiver
     activity
     balance
-    image
+    image File.new(Rails.root + 'spec/fixtures/images/rails.png')
     amount 100
   end
 end
