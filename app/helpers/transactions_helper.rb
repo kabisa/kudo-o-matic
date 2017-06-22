ae0d4cd9aca602d8213aa73a38bb99fcef254644
Egon Meijers <egonmeijers12@gmail.com>
2017-06-22 14:49:48 +0200

    Added Transaction helper for activity

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,9 @@
 module TransactionsHelper
+  include ActionView::Helpers::TextHelper
+
+  def render_activity(transaction)
+    escapteHTML = html_escape(transaction.activity_name_feed)
+    autolink = auto_link(escapteHTML, :all, target: '_blank')
+    @markdown.render(autolink).html_safe
+  end
 end
