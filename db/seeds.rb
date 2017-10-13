2550aca1b7e6d8c2ca61e27ca4aaef2a326db162
Robin Laugs <robinlaugs@live.nl>
2017-10-13 10:48:27 +0200

    Refactored the seeds.rb file and removed unnecessary delete_all calls
    Implemented the creation of the goals as a loop and removed the unnecessary delete_all calls.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-Balance.delete_all
-Goal.delete_all
-Transaction.delete_all
-Activity.delete_all
-User.delete_all
+balance = Balance.create(name: Date.today.year, current: true)
 
-@balance = Balance.find_or_create_by(name: 'New Balance', current: true)
-
-Goal.create(name: "Goal 1", amount: 1500,  balance: @balance, achieved_on: nil)
-Goal.create(name: "Goal 2", amount: 5000, balance: @balance, achieved_on: nil)
-Goal.create(name: "Goal 3", amount: 10000, balance: @balance, achieved_on: nil)
+(1..3).each {|i| Goal.create(name: "Goal #{i}", amount: i * 500, balance: balance)}
