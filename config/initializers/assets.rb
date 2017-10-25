e0696a89fd2414667279d11388ca012fdd018ace
Robin Laugs <robinlaugs@live.nl>
2017-10-25 08:19:53 +0200

    Implemented summary mail and refactored and updated overall mail design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.precompile += %w(*.png)
-Rails.application.config.assets.precompile << 'components/_email-styling.scss'
-
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( *.png )
+Rails.application.config.assets.precompile += %w( email.css )
