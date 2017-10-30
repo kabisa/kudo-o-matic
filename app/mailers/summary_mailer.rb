038c5a39f608f1a0e99800514daa9b4d664b6969
Robin Laugs <robinlaugs@live.nl>
2017-10-30 16:07:16 +0100

    Significantly improved mail compatibility with several mail clients.
    Significantly improved mail compatibility with several mail clients like Outlook and Gmail (and their apps) by rewriting the mails to use tables instead of divs.

--- app/mailers/summary_mailer.rb
+++ app/mailers/summary_mailer.rb
@@ -1,10 +1,9 @@
 class SummaryMailer < ApplicationMailer
   def self.new_summary
     return if Rails.env == 'test' || ENV['MAIL_USERNAME'] == nil
-
     users = User.where.not(email: '').where(mail_notifications: true)
     users.each do |user|
-      summary_email(user).deliver_later
+      summary_email(user).deliver!
     end
   end
 
@@ -15,7 +14,7 @@ class SummaryMailer < ApplicationMailer
     @markdown = Redcarpet::Markdown.new(MdEmoji::Render, :no_intra_emphasis => true)
 
     attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/kudo-o-matic-white-mail.png")
-    attachments.inline['no-picture-icon.jpg'] = File.read("#{Rails.root}/public/no-picture-icon.jpg")
+    attachments.inline['no-picture.jpg'] = File.read("#{Rails.root}/public/no-picture-icon.jpg")
 
     mail(to: user.email, subject: 'Weekly ₭udo summary!')
   end
