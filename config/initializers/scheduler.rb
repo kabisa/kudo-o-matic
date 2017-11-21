930fe70495041e80affded5b0484ab996e0a9aa8
Robin Laugs <robinlaugs@live.nl>
2017-11-21 14:30:43 +0100

    Updated cronjob schedule and adjusted hours for GMT

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,10 +2,10 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler::singleton
 
-scheduler.cron '0 10 * * 5' do
+scheduler.cron '0 9 * * 5' do
   SummaryMailer.new_summary
 end
 
-scheduler.cron '0 10 * * 5' do
+scheduler.cron '55 10 * * 5' do
   SlackService.instance.send_reminder
 end
