a53a49b0ead2717f573803716ba7291b37ad217a
Robin Laugs <robinlaugs@live.nl>
2017-11-29 09:51:20 +0100

    Refactored the ApiController and AuthenticationController

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,22 +1,17 @@
-class Api::V1::AuthenticationController < JSONAPI::ResourceController
-  before_action :set_default_response_format
+class Api::V1::AuthenticationController < Api::V1::ApiController
+  skip_before_action :authorize_request, only: [:obtain_api_token]
 
   def obtain_api_token
-    begin
-      validate_jwt_token(params[:jwt_token])
+    validate_jwt_token(params[:jwt_token])
 
-      @user = User.from_api_token_request(api_token_request_params)
-      render 'api/v1/authentication/api_token', status: :ok
-    rescue => e
-      handle_exceptions(e)
-    end
+    @user = User.from_api_token_request(params)
+
+    render 'api/v1/authentication/obtain_api_token', status: :created
+  rescue => e
+    handle_exceptions(e)
   end
 
   private
-
-  def set_default_response_format
-    request.format = :json
-  end
 
   def validate_jwt_token(jwt_token)
     validator = GoogleIDToken::Validator.new
@@ -26,13 +21,9 @@ class Api::V1::AuthenticationController < JSONAPI::ResourceController
     begin
       validator.check(jwt_token, ENV['GOOGLE_CLIENT_ID_KUDO_O_MOBILE_ANDROID'])
     rescue GoogleIDToken::ValidationError
-      error_object_overrides = {title: 'Invalid JWT Token', detail: 'No valid JWT-token was provided.'}
+      error_object_overrides = {title: 'Unauthorized', detail: 'No valid JSON Web Tokens token was provided.'}
       error = Api::V1::UnauthorizedError.new(error_object_overrides)
       raise error
     end
-  end
-
-  def api_token_request_params
-    params.permit(:jwt_token, :uid, :provider, :name, :email, :avatar_url)
   end
 end
