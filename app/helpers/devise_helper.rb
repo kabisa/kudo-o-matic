6c6b76782b8fedc4eab78e3e840cc7f9ed42eabc
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-04-13 13:45:58 +0200

    Fixed form styling bug after validation

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,14 +4,14 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-      <button type="button" class="close" data-dismiss="alert">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <strong>
-       #{pluralize(resource.errors.count, "error")} must be fixed
-      </strong>
-      #{messages}
+    <div class="message-container show-message" role="alert">
+      <div class="error-message margin-bottom">
+        <span class="message">
+          <ul>
+            #{messages}
+          </ul>
+        </span>
+      </div>
     </div>
     HTML
 
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.default :from => 'support@yourdomain.com'
+
+# Remove field_with_error divs
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
