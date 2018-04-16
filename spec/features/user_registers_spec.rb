8896268d1154b2ba677a6403506d2fe830f467b4
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-04-16 09:27:06 +0200

    Added shared links to sign_in/sign_up, changed register rspec to work with unconfirmed access period

--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'Register', type: :feature do
+  let!(:balance) { create :balance, :current}
+
   scenario 'User signs up' do
     visit '/sign_up'
 
@@ -10,6 +12,6 @@ RSpec.feature 'Register', type: :feature do
     fill_in 'user_password_confirmation', with: 'password'
 
     click_button 'Sign up'
-    expect(current_path).to eq user_session_path
+    expect(current_path).to eq root_path
   end
 end
