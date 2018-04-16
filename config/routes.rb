4e9015e482b19ce498a7fa9c42df5c6529fe24c0
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-04-16 10:48:31 +0200

    Removed custom registration controller because it is no longer needed

--- config/routes.rb
+++ config/routes.rb
@@ -103,8 +103,9 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks',
-                                   registrations: 'registrations'}
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
