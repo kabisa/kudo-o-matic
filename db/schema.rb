4a0a7f79f335e24144fab38ad0d7555a4e77c255
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-05-04 15:36:58 +0200

    Readded admin default to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502095227) do
+ActiveRecord::Schema.define(version: 20180504133504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(version: 20180502095227) do
     t.string   "uid"
     t.string   "avatar_url"
     t.string   "slack_name"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
     t.string   "api_token"
     t.datetime "deactivated_at"
     t.json     "preferences"
