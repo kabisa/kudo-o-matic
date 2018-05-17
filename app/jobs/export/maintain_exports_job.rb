6898b79f4e69e37d42ae7efd546591c431f39a3d
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-05-17 14:33:00 +0200

    Moved file deletion to model

--- app/jobs/export/maintain_exports_job.rb
+++ app/jobs/export/maintain_exports_job.rb
@@ -2,10 +2,6 @@
 
 Export::MaintainExportsJob = Struct.new do
   def perform
-    exports = Export.all_expired
-    exports.each do |e|
-      File.delete(e.zip) if File.exist?(e.zip)
-    end
-    exports.destroy_all
+    Export.all_expired.destroy_all
   end
 end
--- app/models/export.rb
+++ app/models/export.rb
@@ -1,7 +1,14 @@
 class Export < ActiveRecord::Base
   belongs_to :user
+  before_destroy :delete_file
 
   def self.all_expired
     Export.where('created_at < ?', 1.week.ago)
+  end
+
+  private
+
+  def delete_file
+    File.delete(zip) if File.exist?(zip)
   end
 end
