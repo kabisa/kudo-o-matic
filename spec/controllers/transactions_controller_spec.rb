36769fcd80c421163de20c3958f44d0fae2a72f6
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-05-25 14:57:38 +0200

    Added more tests to transactions_controller_spec

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -1,30 +1,55 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe TransactionsController, type: :controller do
-  let!(:user) {create(:user, :admin)}
+  let!(:user) { create(:user, :admin) }
   let!(:team) { create(:team) }
-  let!(:balance) {create :balance, current: :current, team_id: team.id}
-  let!(:goal) {create(:goal)}
-  let!(:transaction) {create(:transaction, team_id: team.id, balance: balance)}
+  let!(:team2) { create(:team, name: 'The Company', slug: 'the-company') }
+  let!(:balance) { create :balance, current: :current, team_id: team.id }
+  let!(:goal) { create(:goal) }
+  let!(:transaction) { create(:transaction, team_id: team.id, balance: balance) }
 
   before do
     team.add_member(user)
     sign_in user
-    @current_team = team.id
   end
 
   describe 'GET #show' do
-    before do
-      get :show, params: {id: transaction.id, tenant: team.slug}
+    context 'with a valid membership' do
+      before do
+        get :show, params: { id: transaction.id, tenant: team.slug }
+      end
+
+      it 'gets show' do
+        expect(response).to render_template(:show)
+      end
+
+      it 'returns a 200 (ok) status code' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it 'gets show' do
-      expect(response).to render_template(:show)
+    context 'with a invalid membership' do
+      before do
+        get :show, params: { id: transaction.id, tenant: team2.slug }
+      end
+
+      it 'gets teams/access_denied' do
+        expect(response).to render_template('teams/access_denied')
+      end
+
+      it 'returns a 403 (forbidden) status code' do
+        expect(response).to have_http_status(403)
+      end
     end
 
-    it 'returns a 200 (ok) status code' do
-      expect(response).to have_http_status(200)
+    context 'with a non-existing team' do
+      it 'raises an ActiveRecord::NotFound error' do
+        expect {
+          get :show, params: { id: transaction.id, tenant: 'non-existing-team' }
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
