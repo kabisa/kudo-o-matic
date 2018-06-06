20054a2f2db4935da4347675f8c76ae9bfa29d7b
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-06-06 11:23:22 +0200

    Added feature tests for inviting users

--- spec/features/invite_user_spec.rb
+++ spec/features/invite_user_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 
 RSpec.feature 'Invite a user', type: :feature do
   let(:user) { create :user }
+  let(:user2) { create :user, name:'Henk', email:'henk@example.com' }
   let(:team) { create :team }
+  let!(:invites_before) { TeamInvite.count }
 
   before do
     team.add_member(user, true)
@@ -16,7 +18,29 @@ RSpec.feature 'Invite a user', type: :feature do
 
     click_link 'Manage Kabisa'
     expect(current_path).to eql('/kabisa/manage')
-    @teams_before = Team.count
   end
 
+  context 'with a valid emailaddress' do
+    before do
+      fill_in 'email-invite-textarea', with: user2.email
+      click_button 'Send invites'
+      expect(current_path).to eql('/kabisa/manage')
+    end
+
+    it 'creates an invite' do
+      expect(TeamInvite.count).not_to eql(invites_before)
+    end
+  end
+
+  context 'with a invalid emailaddress' do
+    before do
+      fill_in 'email-invite-textarea', with: 'novalidemail'
+      click_button 'Send invites'
+      expect(current_path).to eql('/kabisa/manage/invite')
+    end
+
+    it 'does not create an invite' do
+      expect(TeamInvite.count).to eql(invites_before)
+    end
+  end
 end
