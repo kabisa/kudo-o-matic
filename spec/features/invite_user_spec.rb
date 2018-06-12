5d8bb82be0a6d3fd26b8765ef8a92ae19da47651
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-06-12 10:45:12 +0200

    Updated feature spec to show that multiple emails work

--- spec/features/invite_user_spec.rb
+++ spec/features/invite_user_spec.rb
@@ -3,8 +3,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Invite a user', type: :feature do
-  let(:user) { create :user }
-  let(:user2) { create :user, name:'Henk', email:'henk@example.com' }
+  let!(:user) { create :user }
+  let!(:user2) { create :user, name:'Henk', email:'henk@example.com' }
+  let!(:user3) { create(:user, name: 'Jan', email: 'jan@example.com') }
+  let!(:user4) { create(:user, name: 'Rico', email: 'rico@example.com') }
+  let!(:user5) { create(:user, name: 'Ariejan', email: 'ariejan@example.com') }
   let(:team) { create :team }
   let!(:invites_before) { TeamInvite.count }
 
@@ -22,13 +25,14 @@ RSpec.feature 'Invite a user', type: :feature do
 
   context 'with a valid emailaddress' do
     before do
-      fill_in 'email-invite-textarea', with: user2.email
+      emails = 'henk@example.com, jan@example.com; "Rico" <rico@example.com>, Ariejan <ariejan@example.com>'
+      fill_in 'email-invite-textarea', with: emails
       click_button 'Send invites'
       expect(current_path).to eql('/kabisa/manage')
     end
 
     it 'creates an invite' do
-      expect(TeamInvite.count).not_to eql(invites_before)
+      expect(TeamInvite.count).to eql(4)
     end
   end
 
