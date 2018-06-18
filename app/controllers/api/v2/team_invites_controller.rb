b6c638a98d8fc6c4a3650c018b6c4234ec3613a1
Ruud Schroën <ruud.schroen@kabisa.nl>
2018-06-18 13:57:13 +0200

    Replaced where with find in team_invites_controller

--- app/controllers/api/v2/team_invites_controller.rb
+++ app/controllers/api/v2/team_invites_controller.rb
@@ -4,22 +4,20 @@ class Api::V2::TeamInvitesController < Api::V2::ApiController
   def update
     invite_id = params[:id]
     accept = params['accept']
-    @invite = api_user.team_invites.open.where(id: invite_id).first
-    if @invite
-      if accept
-        @invite.accept
-        render 'api/v2/teams/accepted', status: :ok
-      else
-        @invite.decline
-        render 'api/v2/teams/declined', status: :ok
-      end
+    @invite = api_user.team_invites.open.find(invite_id)
+    if accept
+      @invite.accept
+      render 'api/v2/teams/accepted', status: :ok
     else
-      render json: { 'errors': [
-        {
-          title: 'Expired invite',
-          detail: 'This invite does not exist or has expired'
-        }
-      ] }
+      @invite.decline
+      render 'api/v2/teams/declined', status: :ok
     end
+  rescue ActiveRecord::RecordNotFound
+    render json: { 'errors': [
+      {
+        title: 'Expired invite',
+        detail: 'This invite does not exist or has expired'
+      }
+    ] }
   end
 end
