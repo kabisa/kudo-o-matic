0c1cf0e2b6ad1c1f025799219bd877c93cbc88bf
egonm12 <egonmeijers12@gmail.com>
2018-11-05 15:41:42 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
+  unless Rails.env.production?
     mount GraphqlPlayground::Rails::Engine, at: "/graphql/playground", graphql_path: "/graphql"
   end
 
