687553078c8cbdde9ef88dd69b763ad9767973c5
egonm12 <egonmeijers12@gmail.com>
2018-11-11 16:22:34 +0100

    Added more team related fields for specific team queries (#452)
    * Added more team related fields for specific team queries
    
    * Deleted activePosts field since it is not necessary at the moment

--- app/graphql/types/team_type.rb
+++ app/graphql/types/team_type.rb
@@ -13,5 +13,30 @@ module Types
       description "The users that are member of the team"
       resolve ->(obj, args, ctx) { Util::RecordLoader.for(User).load_many(obj.memberships.ids) }
     end
+
+    field :posts, !types[Types::PostType] do
+      description "The posts that belong to the team"
+      resolve ->(obj, args, ctx) { Util::RecordLoader.for(Post).load_many(obj.post_ids) }
+    end
+
+    field :kudosMeters, !types[Types::KudosMeterType] do
+      description "All KudosMeters that belong to the team"
+      resolve ->(obj, args, ctx) { Util::RecordLoader.for(KudosMeter).load_many(obj.kudos_meter_ids) }
+    end
+
+    field :activeKudosMeter, !Types::KudosMeterType do
+      description "The active KudosMeter that belongs to the team"
+      resolve ->(obj, args, ctx) { Util::RecordLoader.for(KudosMeter).load(obj.kudos_meter_ids) }
+    end
+
+    field :goals, !types[Types::GoalType] do
+      description "All goals that belong to the team"
+      resolve ->(obj, args, ctx) { Util::RecordLoader.for(Goal).load_many(obj.goal_ids) }
+    end
+
+    field :activeGoals, !types[Types::GoalType] do
+      description "All goals that belong to the team and are part of the active KudosMeter"
+      resolve ->(obj, args, ctx) { Util::RecordLoader.for(Goal).load_many(obj.active_kudos_meter.goal_ids) }
+    end
   end
 end
--- spec/graphql/types/team_type_spec.rb
+++ spec/graphql/types/team_type_spec.rb
@@ -19,8 +19,33 @@ RSpec.describe Types::TeamType do
     expect(subject).to have_field(:slug).that_returns(!types.String)
   end
 
-  it "has a :memberships field of UserType type" do
-    # Ensure the field is of UserType type
+  it "has a :memberships field of an array UserType! type" do
+    # Ensure the field is of array UserType type
     expect(subject).to have_field(:memberships).that_returns(!types[Types::UserType])
+  end
+
+  it "has a :posts field of an array PostType! type" do
+    # Ensure the field is of array PostType type
+    expect(subject).to have_field(:posts).that_returns(!types[Types::PostType])
+  end
+
+  it "has a :kudosMeters field of an array KudosMeterType! type" do
+    # Ensure the field is of array KudosMeterType type
+    expect(subject).to have_field(:kudosMeters).that_returns(!types[Types::KudosMeterType])
+  end
+
+  it "has a :activeKudosMeter field of KudosMeterType! type" do
+    # Ensure the field is of KudosMeterType type
+    expect(subject).to have_field(:activeKudosMeter).that_returns(!Types::KudosMeterType)
+  end
+
+  it "has a :goals field of an array GoalType! type" do
+    # Ensure the field is of array KudosMeterType type
+    expect(subject).to have_field(:kudosMeters).that_returns(!types[Types::KudosMeterType])
+  end
+
+  it "has a :activeGoals field of an array GoalType! type" do
+    # Ensure the field is of KudosMeterType type
+    expect(subject).to have_field(:activeGoals).that_returns(!types[Types::GoalType])
   end
 end
