1ed8f5171cf1005eacb1871dd11f5567bbe1ac7d
Matthijs Groen <matthijs.groen@gmail.com>
2024-10-09 11:21:20 +0200

    Fix creating goal

--- app/graphql/types/goal_type.rb
+++ app/graphql/types/goal_type.rb
@@ -14,9 +14,9 @@ module Types
     field :achieved_on,  GraphQL::Types::ISO8601DateTime,
           null: true,
           description: 'The date the goal is achieved'
-    field :kudosMeter, Types::KudosMeterType,
+    field :kudos_meter, Types::KudosMeterType,
           null: false,
-          description: 'The kudos meter the goal belongs to',
-          resolve: ->(obj, _args, _ctx) { Util::RecordLoader.for(KudosMeter).load(obj.kudos_meter_id) }
+          description: 'The kudos meter the goal belongs to'
+    #     resolve: ->(obj, _args, _ctx) { Util::RecordLoader.for(KudosMeter).load(obj.kudos_meter_id) }
   end
 end
