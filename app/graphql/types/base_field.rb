09b00399eeca25353d93bae1268fea2e899da984
Matthijs Groen <matthijs.groen@gmail.com>
2024-10-10 13:24:52 +0200

    Only check wildcard rights when there is no specific field policy

--- app/graphql/types/base_field.rb
+++ app/graphql/types/base_field.rb
@@ -7,13 +7,13 @@ class Types::BaseField < GraphQL::Schema::Field
       if policy
         authorized = policy.call(obj, args, ctx)
         raise GraphQL::ExecutionError.new("Not authorized to access #{owner.graphql_name}.#{name}") unless authorized
-      end
-
-      # Object type generic policy
-      policy = Util::GraphqlPolicy.guard(owner, :'*')
-      if policy
-        authorized = policy.call(obj, args, ctx)
-        raise GraphQL::ExecutionError.new("Not authorized to access #{owner.graphql_name}.#{name}") unless authorized
+      else
+        # Object type generic policy
+        policy = Util::GraphqlPolicy.guard(owner, :'*')
+        if policy
+          authorized = policy.call(obj, args, ctx)
+          raise GraphQL::ExecutionError.new("Not authorized to access #{owner.graphql_name}.#{name}") unless authorized
+        end
       end
     end
 
