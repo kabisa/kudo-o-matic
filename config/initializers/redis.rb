cfa0637486e68d8ba7e0f4fed2ceb229424a004a
Aschwin Bruyning <aschwinbruyning@outlook.com>
2024-10-29 20:33:25 +0100

    remove loggers for client

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,16 +5,4 @@ Rails.logger.info("Redis initializer is being loaded")
 # Log Redis connection details
 Rails.logger.info("REDIS_URL: #{ENV['REDIS_URL']}")
 
-# Conditional block to prevent Redis commands during asset precompilation
-unless ENV['ASSET_PRECOMPILE']
-  # Log connection details if not in asset precompilation mode
-  Rails.logger.info("Redis URL: #{$redis.client.options[:url]}")
-  Rails.logger.info("SSL Params: #{$redis.client.options[:ssl_params]}")
-
-  # Test Redis connection with a PING command
-  begin
-    Rails.logger.info("Redis PING: #{$redis.ping}")
-  rescue StandardError => e
-    Rails.logger.error("Redis connection failed: #{e.message}")
-  end
-end
+Rails.logger.info("SSL Params: #{$redis.client.options[:ssl_params]}")
