04f26512696099605828ac4dc63053cdcbc6308c
Aschwin Bruyning <aschwinbruyning@outlook.com>
2024-10-29 20:36:01 +0100

    remove loggers for client

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,3 @@
 # Initialize Redis connection
 $redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 Rails.logger.info("Redis initializer is being loaded")
-
-# Log Redis connection details
-Rails.logger.info("REDIS_URL: #{ENV['REDIS_URL']}")
-
-Rails.logger.info("SSL Params: #{$redis.client.options[:ssl_params]}")
