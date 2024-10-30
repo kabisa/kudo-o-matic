735584b776e3b43f63078ca507bece0237e49ddc
Pascal Widdershoven <pascal@kabisa.nl>
2024-10-30 21:04:18 +0100

    Disable SSL verification for Redis as per Heroku requirements

--- config/initializers/redis.rb
+++
@@ -1,3 +0,0 @@
-# Initialize Redis connection
-$redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-Rails.logger.info("Redis initializer is being loaded")
