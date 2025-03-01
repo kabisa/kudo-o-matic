13e2026b146842e7500e1b35dbdc198eb4206a71
Joost Saanen <joost@kabisa.nl>
2025-03-01 12:51:54 +0100

    Fix policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,30 +18,28 @@ resource "aws_iam_policy" "kudo_policy" {
   name        = "kudo-s3-access-policy"
   description = "Policy to allow full access to kudo S3 buckets"
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "s3:ListAllMyBuckets",
-            "Resource": "arn:aws:s3:::*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": [
-                aws_s3_bucket.kudo_o_matic_development.arn,
-                "${aws_s3_bucket.kudo_o_matic_development.arn}/*",
-                aws_s3_bucket.kudo_o_matic_staging.arn,
-                "${aws_s3_bucket.kudo_o_matic_staging.arn}/*",
-                aws_s3_bucket.kudo_o_matic_production.arn,
-                "${aws_s3_bucket.kudo_o_matic_production.arn}/*"
-            ]
-        }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "s3:ListAllMyBuckets"
+        Resource = "arn:aws:s3:::*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "s3:*"
+        Resource = [
+          aws_s3_bucket.kudo_o_matic_development.arn,
+          "${aws_s3_bucket.kudo_o_matic_development.arn}/*",
+          aws_s3_bucket.kudo_o_matic_staging.arn,
+          "${aws_s3_bucket.kudo_o_matic_staging.arn}/*",
+          aws_s3_bucket.kudo_o_matic_production.arn,
+          "${aws_s3_bucket.kudo_o_matic_production.arn}/*"
+        ]
+      }
     ]
-}
-POLICY
+  })
 }
 
 resource "aws_iam_user_policy_attachment" "kudo_user_policy_attachment" {
